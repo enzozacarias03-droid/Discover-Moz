9686c8c013c3e4d8d6e458f41623ce49cc7e26eb
Enzo <enzozacarias03@gmail.com>
2026-04-05 21:07:15 +0200

    Fix STATIC_ROOT path for Railway deployment

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -155,11 +155,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# /data/static/media
-STATIC_ROOT = DATA_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-# /data/web/media
 MEDIA_ROOT = DATA_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
