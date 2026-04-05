2e3fa602521971aba4eed635fa5b5cd1f9c0bd9e
Enzo <enzozacarias03@gmail.com>
2026-04-05 20:48:57 +0200

    Fix Cloudinary app order in INSTALLED_APPS

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -50,8 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
 
     # My apps
